846ca78327a93d43db664a55fbe3d8d5d2245c8c
Felipe Mendes Braga <felipe.mendes.braga@gmail.com>
2016-04-05 15:17:47 -0300

    mapa agora pega o ponto geográfico do dispositivo

--- index.android.js
+++ index.android.js
@@ -13,6 +13,12 @@ class Weather extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            region: {
+              latitude: 0,
+              longitude: 0,
+              latitudeDelta: 50,
+              longitudeDelta: 50
+            },
             marker: {
                 latitude: 37,
                 longitude: -95
@@ -22,19 +28,35 @@ class Weather extends Component {
             description: ''
         };
     }
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition((position) => {
+          this.setState(
+            {
+              region: {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+                latitudeDelta: 50,
+                longitudeDelta: 50
+              }
+            }
+          );
+        });
+    }
+
     render() {
 
         return (
             <View style={styles.container}>
-              <View style={styles.mapWrapper}>
-                <MapView style={styles.map} onRegionChangeComplete={this.onRegionChangeComplete.bind(this)}>
-                    <MapView.Marker coordinate={this.state.marker} title={this.state.city} description={this.state.description}/>
-                </MapView>
-              </View>
+                <View style={styles.mapWrapper}>
+                    <MapView region={this.state.region} style={styles.map} onRegionChangeComplete={this.onRegionChangeComplete.bind(this)}>
+                        <MapView.Marker coordinate={this.state.marker} title={this.state.city} description={this.state.description}/>
+                    </MapView>
+                </View>
                 <View style={styles.textWrapper}>
-                  <Text style={styles.text}>{this.state.city}</Text>
-                  <Text style={styles.text}>{this.state.temperature}</Text>
-                  <Text style={styles.text}>{this.state.description}</Text>
+                    <Text style={styles.text}>{this.state.city}</Text>
+                    <Text style={styles.text}>{this.state.temperature}</Text>
+                    <Text style={styles.text}>{this.state.description}</Text>
                 </View>
             </View>
         )
@@ -49,7 +71,6 @@ class Weather extends Component {
         });
 
         Api(region.latitude, region.longitude).then((data) => {
-          console.log(data);
             this.setState(data);
         });
     }
@@ -63,14 +84,14 @@ const styles = StyleSheet.create({
         backgroundColor: '#F5FCFF'
     },
     mapWrapper: {
-      flex: 2
+        flex: 2
     },
     textWrapper: {
-      flex: 1,
-      alignItems: 'center'
+        flex: 1,
+        alignItems: 'center'
     },
     text: {
-      fontSize: 30
+        fontSize: 30
     },
     map: {
         position: 'absolute',
